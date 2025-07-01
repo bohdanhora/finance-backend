ae1112cf531ff426b2910da74617e2001e3c5bcd
Bohdan Hora <bohdan.hora.developer@gmail.com>
2025-07-02 02:13:23 +0300

    registration, login, refreshtoken, guard

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegistrationDto } from './dtos/registration.dto';
+import { LoginDto } from './dtos/login.dto';
+import { RefreshTokenDto } from './dtos/refresh-token.dto';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('registration')
+    async registration(@Body() registationData: RegistrationDto) {
+        return this.authService.registration(registationData);
+    }
+
+    @Post('login')
+    async login(@Body() loginData: LoginDto) {
+        return this.authService.login(loginData);
+    }
+
+    @Post('refresh')
+    async refreshTokens(@Body() refreshTokenData: RefreshTokenDto) {
+        return this.authService.refreshTokens(refreshTokenData.refreshToken);
+    }
+}
